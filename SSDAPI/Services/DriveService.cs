b11dc7fabddaeccecb3b168d7d6ef08fa7427d1e
Dewsara <dewsara6@gmail.com>
2021-10-03 17:19:20 +0530

    Adding DriveService Class and Token Autherization

--- SSDAPI/Services/DriveService.cs
+++ SSDAPI/Services/DriveService.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using static Google.Apis.Drive.v3.DriveService;
 
 namespace SSDAPI.Services
 {
     public class DriveService
     {
+        public DriveService GetService(string accTkn, string logEmail)
+        {
+            var tokenResponse = new TokenResponse
+            {
+                AccessToken = accTkn,
+                RefreshToken = "1//04q1DOu87Mh7-CgYIARAAGAQSNwF-L9IrDwXy_xi2O8sfQk0SHsPcWoeV5pRTmBMLJ85vlISi2ETlM1mIREE9_eGQiNPrIMX-acE"
+            };
+
+            var applicationName = "SSD Assignment 2";// Use the name of the project in Google Cloud
+            var username = logEmail;//"it18146516@my.sliit.lk"; // Use your email
+        }
+        }
     }
-}
