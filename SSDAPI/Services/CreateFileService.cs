65654947c6c5c70884cb13f9b9d3a94845f91569
Yuvin Ransika <yuvinransika1998@gmail.com>
2021-10-03 17:51:51 +0530

    image upload

--- SSDAPI/Services/CreateFileService.cs
+++ SSDAPI/Services/CreateFileService.cs
@@ -24,7 +24,26 @@ namespace SSDAPI.Services
 
                 byte[] bytes = Convert.FromBase64String(modifiedstream);
                 var requestID = "";
-               
+                using (Stream ms = new MemoryStream(bytes))
+                {
+                    DriveAccessService dv = new DriveAccessService();
+                    DriveService service = dv.GetService(imageModel.Token,imageModel.Email);
+
+
+                    var driveFile = new Google.Apis.Drive.v3.Data.File();
+                    driveFile.Name = FileName;
+                    driveFile.Description = "Upload By Admin";
+                    driveFile.MimeType = "image/png";
+                    driveFile.Parents = new string[] { "root" };
+
+
+                    var request = service.Files.Create(driveFile, ms, "image/png");
+                    request.Fields = "id";
+                    requestID = "12";
+                    var response = request.Upload();
+                    if (response.Status != Google.Apis.Upload.UploadStatus.Completed)
+                        throw response.Exception;
+                }
                 return requestID;
             }
             catch (Exception ex)
